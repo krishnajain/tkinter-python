b7d8b8fef93c387bf7c74ffa5fa89f08daed838a
krishnajain <69294021+krishnajain@users.noreply.github.com>
2021-03-23 13:17:51 +0530

    Add files via upload

---
+++ tkinter button widget/tkinter button widget.py
@@ -0,0 +1,8 @@
+from tkinter import *   
+root = Tk()              
+ 
+root.geometry('100x100') 
+b = Button(root, text = 'Click me !', bd = '5',
+                          command = root.destroy)    
+b.pack(side = 'top')    
+root.mainloop()
